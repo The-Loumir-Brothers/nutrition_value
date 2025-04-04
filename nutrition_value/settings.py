022bddb0646c4ed4c81e56908c58b6f9696a4c54
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2025-04-04 02:13:06 -0400

    added code to settings.py to be able to use images and created a static folder

--- nutrition_value/settings.py
+++ nutrition_value/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'your_app/static')
+]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
