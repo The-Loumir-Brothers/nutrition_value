13c31966c0885277d786667382cc0d5fb327db7a
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2025-04-10 19:42:52 -0400

    created gitignore file; added secret key to it

--- nutrition_value/settings.py
+++ nutrition_value/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from .top_secret import SECRET_KEY
 import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -28,8 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3_&7#t9jq4t1yavfa!+4npz=6rgyz*s+=((&b6$yqkouzcnsg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ nutrition_value/top_secret.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-m3_$!!x%3f!(hbd54o8nd9*kqrrpqfa2din6$yx7sz#rw_%k2m9r+'
